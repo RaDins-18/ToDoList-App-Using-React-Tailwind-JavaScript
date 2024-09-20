b50d6968fe5b35b25c4166f09a51b37a69e62d19
RaDin <143905822+RaDins-18@users.noreply.github.com>
2024-09-20 19:44:21 +0500

    make changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = ({theme, setTheme}) => {
     return (
         <nav className='navbar w-full flex flex-col sm:flex-row h-20 justify-between items-end sm:items-center sm:gap-4 absolute'>
             <div className="start h-20 w-[20%]">
-                <a href="https://github.com/RaDins-18" target='_blank' className="github-corner">
+                <a href="https://github.com/RaDins-18/ToDoList-App-Using-React-Tailwind-JavaScript" target='_blank' className="github-corner">
                     <svg width="80" height="80" viewBox="0 0 250 250" id="github" className="pointer">
                         <path fill={Themes[theme].bg} d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path>
                         <path fill={Themes[theme].color1} d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" id="octo-tail"></path>
